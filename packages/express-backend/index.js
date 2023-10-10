152ac61f0bff96b8aa3396052590e5570cb2583d
manvsatish <manvsatish@gmail.com>
2023-10-09 18:36:41 -0700

    most recent

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -52,17 +52,17 @@ const findUserByName = (name) => {
         .filter( (user) => user['name'] === name); 
 }
 
-app.get('/users', (req, res) => {
-    const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByName(name);
-        result = {users_list: result};
-        res.send(result);
-    }
-    else{
-        res.send(users);
-    }
-});
+// app.get('/users', (req, res) => {
+//     const name = req.query.name;
+//     if (name != undefined){
+//         let result = findUserByName(name);
+//         result = {users_list: result};
+//         res.send(result);
+//     }
+//     else{
+//         res.send(users);
+//     }
+// });
 
 const findUserById = (id) =>
     users['users_list']
@@ -75,5 +75,73 @@ app.get('/users/:id', (req, res) => {
         res.status(404).send('Resource not found.');
     } else {
         res.send(result);
+    }
+});
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+const findUsersByNameAndJob = (name, job) => {
+    if (name && job) {
+        return users['users_list']
+            .filter( (user) => user['name'] === name && user['job'] === job);
+    } else {
+        return null;  // Indicates that the proper parameters were not provided.
+    }
+}
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+
+    // If both name and job are provided, find by both.
+    if (name && job) {
+        let result = findUsersByNameAndJob(name, job);
+        if (result && result.length > 0) {
+            res.send({users_list: result});
+        } else {
+            res.status(404).send('No users found with the provided name and job.');
+        }
+    }
+    // If only name is provided, find by name.
+    else if (name) {
+        let result = findUserByName(name);
+        if (result && result.length > 0) {
+            res.send({users_list: result});
+        } else {
+            res.status(404).send('No users found with the provided name.');
+        }
+    }
+    // If neither name nor job is provided, return all users.
+    else {
+        res.send(users);
+    }
+});
+
+const deleteUser = (user) => {
+    const index = users['users_list'].findIndex(user => user['id'] === id);
+    if (index !== -1) {
+        users['users_list'].splice(index, 1);
+        return true;  // Indicates the user was found and deleted.
+    }
+    return false;  // Indicates the user was not found.
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const wasDeleted = deleteUserById(id);
+
+    if (wasDeleted) {
+        res.send({ message: 'User deleted successfully.' });
+    } else {
+        res.status(404).send({ message: 'User not found.' });
     }
 });
