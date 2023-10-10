4ce955c405ae2d9746af1c9c4f9922ffb486677d
manvsatish <manvsatish@gmail.com>
2023-10-09 20:05:35 -0700

    most recent files!

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -6,15 +6,50 @@ function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter (index) {
-	    const updated = characters.filter((character, i) => {
-	        return i !== index
-	    });
-	  setCharacters(updated);
-	}
-
-    function updateList(person) {
-        setCharacters([...characters, person]);
+        const userToDelete = characters[index];
+        fetch(`http://localhost:8000/users/${userToDelete.id}`, {
+            method: 'DELETE'
+        })
+        .then(res => {
+            if (res.status === 204) {  // Successfully deleted
+                const updated = characters.filter((character, i) => {
+                    return i !== index;
+                });
+                setCharacters(updated);
+                console.log('Successfully deleted the user.');
+            } else {
+                console.error('Failed to delete user.');
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
+
+    function updateList(person) { 
+        postUser(person)
+            .then(res => {
+                if (res.status !== 201) {
+                    throw new Error('Insertion failed on the backend');
+                }
+                // Check if content type is JSON before attempting to parse
+                const contentType = res.headers.get("content-type");
+                if (contentType && contentType.indexOf("application/json") !== -1) {
+                    return res.json();
+                }
+                throw new Error('Received non-JSON response from the server');
+            })
+            .then(data => {
+                if (data && data.id) {
+                    setCharacters(prevCharacters => [...prevCharacters, data]);
+                    console.log('User added successfully with ID:', data.id);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }    
+    
 
     function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
@@ -31,14 +66,6 @@ function MyApp() {
         });
     
         return promise;
-    }
-
-    function updateList(person) { 
-        postUser(person)
-          .then(() => setCharacters([...characters, person]))
-          .catch((error) => {
-            console.log(error);
-          })
     }
 
     useEffect(() => {
--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -4,6 +4,7 @@ function TableHeader() {
     return (
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Job</th>
             </tr>
@@ -15,13 +16,14 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
+            <td>{row.id}</td> 
             <td>{row.name}</td>
             <td>{row.job}</td>
             <td>
-                    <button onClick={() => 
-                        props.removeCharacter(index)}>
-                        Delete
-                    </button>
+                <button onClick={() => 
+                    props.removeCharacter(index)}>
+                    Delete
+                </button>
             </td>
         </tr>
       );
@@ -30,7 +32,7 @@ function TableBody(props) {
     return (
         <tbody>
           {rows}
-         </tbody>
+        </tbody>
     );
 }
   
